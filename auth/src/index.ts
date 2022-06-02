07f469906e0282db15c0ca991641ae79d806541a
Tim Butorac <tim@timbutorac.com>
2022-06-02 07:53:00 -0700

    not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response } from 'express';
-import { json } from 'body-parser';
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/sign-in';
-import { signOutRouter } from './routes/sign-out';
-import { signUpRouter } from './routes/sign-up';
-import { errorHandler } from './middlewares/error-handler';
+import express, { Request, Response } from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/sign-in";
+import { signOutRouter } from "./routes/sign-out";
+import { signUpRouter } from "./routes/sign-up";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -15,6 +17,11 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+// handle undefined routes
+app.all('*', () => {
+  throw new NotFoundError();
+});
 
 // middlewares
 app.use(errorHandler);
