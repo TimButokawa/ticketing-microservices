38f2fc348bb282b5fa58b760a01441f2ae8da4e1
Tim Butorac <timbutorac@gmail.com>
2022-06-18 14:26:29 -0700

    revert next removal

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { CustomError } from '../errors/custom-error';
 
-export const errorHandler = (err: Error, req: Request, res: Response) => {
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({
       errors: err.serializeErrors(),
