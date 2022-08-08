3881c390c7eaf7b8ec7c920707411fd0439906f0
Tim Butorac <timbutorac@gmail.com>
2022-08-07 18:14:56 -0700

    app init refactor - setup tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,7 @@
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
-import express from 'express';
-import 'express-async-errors';
+
 import mongoose from 'mongoose';
 import { DatabaseConnectionError } from './errors/database-connection-error';
-import { NotFoundError } from './errors/not-found-error';
-import { errorHandler } from './middlewares/error-handler';
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/sign-in';
-import { signOutRouter } from './routes/sign-out';
-import { signUpRouter } from './routes/sign-up';
-
-const app = express();
-
-// trust traffic from nginx
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: true,
-}));
-
-// routes
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-// handle undefined routes
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-// middlewares
-app.use(errorHandler);
+import { app } from './app';
 
 const startUp = () => {
   if (!process.env.JWT_KEY) {
