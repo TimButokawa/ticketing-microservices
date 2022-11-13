a421a6d8c0675efb5c4c3d417e73cbf0ace996ed
Tim Butorac <timbutorac@gmail.com>
2022-11-12 17:44:57 -0800

    add create/updated ticket publishers

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,9 +1,9 @@
-
 import mongoose from 'mongoose';
 import { DatabaseConnectionError } from '@tbticketsplease/common';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
-const startUp = () => {
+const startUp = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
@@ -13,7 +13,16 @@ const startUp = () => {
   }
 
   try {
-    mongoose.connect(process.env.MONGO_URI);
+    await natsWrapper.connect('ticketing', 'random-string', 'http://nats-ip-service:4222');
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closing...');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to auth db');
   } catch (e) {
     console.log('error: ', e)
