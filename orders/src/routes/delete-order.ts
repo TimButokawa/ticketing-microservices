9184ecc903dc1b410b7b0288517939fb137d766c
Tim Butorac <timbutorac@gmail.com>
2022-11-20 10:18:22 -0800

    update routes

--- orders/src/routes/delete-order.ts
+++ orders/src/routes/delete-order.ts
@@ -1,9 +1,37 @@
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth } from '@tbticketsplease/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+// actually just patching the status property
+router.delete('/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    if (!orderId) {
+      throw new BadRequestError('Must provide orderId');
+    }
+
+    const order = await Order.findById(orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotFoundError();
+    }
+
+    order.status = OrderStatus.Canceled;
+
+    await order.save();
+
+    // TODO: publish an event for order created
+
+    res.status(200).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
--- orders/src/routes/get-order.ts
+++ orders/src/routes/get-order.ts
@@ -1,9 +1,30 @@
+import { BadRequestError, NotAuthorizedError, NotFoundError, requireAuth } from '@tbticketsplease/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get('/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    if (!orderId) {
+      throw new BadRequestError('Must provide orderId');
+    }
+
+    const order = await Order.findById(orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotFoundError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as getOrderRouter };
