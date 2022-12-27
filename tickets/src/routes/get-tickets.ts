58a9f94196e7f000d628fb911595bc3c0aa99e55
Tim Butorac <timbutorac@gmail.com>
2022-12-27 10:42:26 -0800

    adjust expiration

--- tickets/src/routes/get-tickets.ts
+++ tickets/src/routes/get-tickets.ts
@@ -5,12 +5,13 @@ import { Ticket } from '../models/ticket';
 const router = express.Router();
 
 router.get('/api/tickets',
-    requireAuth,
-    async (req: Request, res: Response) => {
-    const tickets = await Ticket.find({});
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const tickets = await Ticket.find({
+      orderId: undefined
+    });
 
     res.send(tickets);
 });
 
 export { router as getTicketsRouter };
-
